2e5201436d03f39a3796019eac920f492c5a26d1
xulingqi0579111 <2867646659@qq.com>
2022-12-21 20:17:58 +0800

    12323

---
+++ Maven-app/src/main/java/org/example/Test1.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.printf("11");
+        System.out.println("22");
+        System.out.printf("");
+        System.out.printf("");
+        System.out.println("222");
+    }
+}
